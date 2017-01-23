cd246ae9d492865df142e9599fe4a08a1ecbfb90
Olli <oltsuwh@gmail.com>
2017-01-23 20:36:51 +0200

    muokattiin piirrettavaa

--- Pongbreaker/src/main/java/pongbreaker/domain/Piirrettava.java
+++ Pongbreaker/src/main/java/pongbreaker/domain/Piirrettava.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pongbreaker.domain;
 
-/**
- *
- * @author wolli
- */
-public class Piirrettava {
-    
+import java.awt.Graphics;
+
+public abstract class Piirrettava {
+
+    protected int x;
+    protected int y;
+
+    public Piirrettava(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public abstract void piirra(Graphics g);
+
 }

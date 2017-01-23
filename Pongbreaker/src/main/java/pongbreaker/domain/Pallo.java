1962c19b78ae1836645124ec2012e3883a6609a2
Olli <oltsuwh@gmail.com>
2017-01-23 21:18:52 +0200

    muokattiin palloa

--- Pongbreaker/src/main/java/pongbreaker/domain/Pallo.java
+++ Pongbreaker/src/main/java/pongbreaker/domain/Pallo.java
@@ -4,48 +4,49 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Pallo extends Piirrettava {
+
     private int r;
-    
+
     private int xNopeus;
     private int yNopeus;
-    
+
     public Pallo(int r, int x, int y) {
         super(x, y);
         this.r = r;
-        
+
         xNopeus = 2;
         yNopeus = 2;
     }
-    
+
     public void liiku() {
         super.x += xNopeus;
         super.y += yNopeus;
     }
-    
+
     public void kaannaXNopeus() {
         xNopeus *= -1;
     }
-    
+
     public void kaannaYNopeus() {
         yNopeus *= -1;
     }
-    
-    public int getR() {
-        return r;
-    }
-    
+
     public int getXNopeus() {
         return xNopeus;
     }
-    
+
     public int getYNopeus() {
         return yNopeus;
+    }
+
+    public int getR() {
+        return r;
     }
 
     @Override
     public void piirra(Graphics g) {
         g.setColor(Color.white);
-        g.fillOval(x - r, y - r, 2*r, 2*r);
+        g.fillOval(x - r, y - r, 2 * r, 2 * r);
     }
 
 }

79cd2d16014e22634ff1aaadc9b9563d8616b443
Olli <oltsuwh@gmail.com>
2017-01-30 17:24:08 +0200

    Lisättiin pelaaja

---
+++ Pongbreaker/src/main/java/pongbreaker/peli/Pelaaja.java
@@ -0,0 +1,36 @@
+package pongbreaker.peli;
+
+import pongbreaker.domain.Maila;
+
+public class Pelaaja {
+
+    private Maila maila;
+
+    private double nopeus;
+    private double kiihtyvyys;
+
+    public Pelaaja(Maila maila) {
+        this.maila = maila;
+        this.nopeus = 5;
+        this.kiihtyvyys = 0;
+    }
+
+    public void liiku() {
+        maila.setY((int) (nopeus * kiihtyvyys));
+        if (this.kiihtyvyys > 0) {
+            if (this.kiihtyvyys - 1 < 0) {
+                this.kiihtyvyys = 0;
+            } else {
+                this.kiihtyvyys -= 1;
+            }
+
+        } else if (this.kiihtyvyys < 0) {
+            if (this.kiihtyvyys + 1 > 0) {
+                this.kiihtyvyys = 0;
+            } else {
+                this.kiihtyvyys += 1;
+            }
+
+        }
+    }
+}

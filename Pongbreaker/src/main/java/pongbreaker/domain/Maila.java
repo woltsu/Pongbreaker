64c51ac7e58bed6152b21b27d33369a73593da81
Olli <oltsuwh@gmail.com>
2017-01-30 17:13:55 +0200

    Maila

--- Pongbreaker/src/main/java/pongbreaker/domain/Maila.java
+++ Pongbreaker/src/main/java/pongbreaker/domain/Maila.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pongbreaker.domain;
 
-/**
- *
- * @author wolli
- */
-public class Maila {
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Maila extends Piirrettava {
+    private int leveys;
+    private int korkeus;
+    
+    public Maila(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void piirra(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect(x - leveys / 2, y - korkeus / 2, leveys, korkeus);
+    }
     
 }

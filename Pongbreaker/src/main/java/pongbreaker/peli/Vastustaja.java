60e53b1164a86a0081e44f40803e68ba25c334d3
Olli <oltsuwh@gmail.com>
2017-02-02 23:00:03 +0200

    refaktorointia

--- Pongbreaker/src/main/java/pongbreaker/peli/Vastustaja.java
+++ Pongbreaker/src/main/java/pongbreaker/peli/Vastustaja.java
@@ -18,33 +18,33 @@ public class Vastustaja {
 
     public void liiku() {
         if (maila.getY() < seurattavaPallo.getY()) {
-            
+
             if (Math.abs(seurattavaPallo.getY() - maila.getY()) > 5) {
-                maila.setY(maila.getY() + (int)(kiihtyvyys * 5));
-            
+                maila.setY(maila.getY() + (int) (kiihtyvyys * 5));
+
             } else {
                 maila.setY(maila.getY() + Math.abs(seurattavaPallo.getY() - maila.getY()));
-                
+
             }
 
         } else if (maila.getY() > seurattavaPallo.getY()) {
             if (Math.abs(seurattavaPallo.getY() - maila.getY()) > 5) {
-                maila.setY(maila.getY() - (int)(kiihtyvyys * 5));
-            
+                maila.setY(maila.getY() - (int) (kiihtyvyys * 5));
+
             } else {
                 maila.setY(maila.getY() - Math.abs(seurattavaPallo.getY() - maila.getY()));
-                
+
             }
 
         }
-        
+
         maila.getHitbox().setLocation(maila.getX() - maila.getLeveys() / 2, maila.getY() - maila.getKorkeus() / 2);
     }
 
     public Maila getMaila() {
         return maila;
     }
-    
+
     public void setKiihtyvyys(double kiihtyvyys) {
         this.kiihtyvyys = kiihtyvyys;
     }

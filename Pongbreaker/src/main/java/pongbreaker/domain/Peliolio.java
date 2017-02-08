5001db53f8395dd6c7f1759ce4b7074ccdb0faed
Olli <oltsuwh@gmail.com>
2017-02-08 15:12:40 +0200

    Lisättiin luokille kuvauksia

--- Pongbreaker/src/main/java/pongbreaker/domain/Peliolio.java
+++ Pongbreaker/src/main/java/pongbreaker/domain/Peliolio.java
@@ -3,10 +3,23 @@ package pongbreaker.domain;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+/**
+ * Luokka kuvaa pelissä esiintyvää oliota, joka reagoi muihin peliolioihin sekä piirretään
+ * peliin.
+ * @author wolli
+ */
+
 public abstract class Peliolio {
 
     protected int x;
     protected int y;
+    
+    /**
+     * Luokan konstruktori.
+     * Asettaa x- ja y-arvot, jotka kuvaavat pistettä koordinaatistossa.
+     * @param x pisteen x-arvo.
+     * @param y pisteen y-arvo.
+     */
 
     public Peliolio(int x, int y) {
         this.x = x;
@@ -28,9 +41,27 @@ public abstract class Peliolio {
     public void setY(int y) {
         this.y = y;
     }
-
+    
+    /**
+     * Metodi piirtää halutun grafiikan ruudulle.
+     * @param g grafiikka, jota käytetään piirtämiseen.
+     */
+    
     public abstract void piirra(Graphics g);
+    
+    /**
+     * Metodi sisältää tiedon siitä, kuinka perivän luokan tulee käyttäytyä,
+     * kun toisen peliolion hitbox osuu luokan omaan hitboxiin.
+     * @see pongbreaker.peli.TormayksienHavaitsija#tarkistaTormaykset() 
+     */
+    
     public abstract void reagoiOsumaan();
+    
+    /**
+     * Palauttaa Rectangle-olion, joka kuvaa peliolion hitboxia.
+     * @return Rectangle-olio, joka olion luokan hitboxia.
+     */
+    
     public abstract Rectangle getHitbox();
 
 }

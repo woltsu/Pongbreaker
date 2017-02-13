256c7cfbda366dd9d0c3edf640ca46af031a2983
Olli <oltsuwh@gmail.com>
2017-02-14 00:22:40 +0200

    refaktorointia

--- Pongbreaker/src/main/java/pongbreaker/domain/Laatikko.java
+++ Pongbreaker/src/main/java/pongbreaker/domain/Laatikko.java
@@ -46,9 +46,9 @@ public class Laatikko extends Peliolio {
             double luku = random.nextDouble();
             if (luku < 0.45) {
                 this.powerUp = PowerUp.MAILA_KASVAA;
-            } else if (luku < 0.85) {
+            } else if (luku < 0.82) {
                 this.powerUp = PowerUp.MAILA_NOPEUTUU;
-            } else if (luku < 0.92) {
+            } else if (luku < 0.95) {
                 this.powerUp = PowerUp.RESETOI_POWERUPIT;
             } else {
                 this.powerUp = PowerUp.TUHOUTUMATON_PALLO;

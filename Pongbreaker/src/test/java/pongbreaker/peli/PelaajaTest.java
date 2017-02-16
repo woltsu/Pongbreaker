71c4297ef61736b735de33156c8140c89b428e67
Olli <oltsuwh@gmail.com>
2017-02-16 19:08:37 +0200

    testejä

--- Pongbreaker/src/test/java/pongbreaker/peli/PelaajaTest.java
+++ Pongbreaker/src/test/java/pongbreaker/peli/PelaajaTest.java
@@ -9,8 +9,11 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import pongbreaker.domain.Maila;
+import pongbreaker.domain.Pallo;
+import pongbreaker.domain.PowerUp;
 
 public class PelaajaTest {
+
     Pelaaja pelaaja;
     Maila maila;
 
@@ -19,7 +22,7 @@ public class PelaajaTest {
         pelaaja = new Pelaaja(new Maila(100, 100));
         maila = pelaaja.getMaila();
     }
-    
+
     @Test
     public void eiVoiLiikkuaJosKiihtyvyysOnNolla() {
         pelaaja.liiku();
@@ -28,7 +31,7 @@ public class PelaajaTest {
         assertEquals(100 - maila.getLeveys() / 2, maila.getHitbox().x);
         assertEquals(100 - maila.getKorkeus() / 2, maila.getHitbox().y);
     }
-    
+
     @Test
     public void liikkuminenYlosToimiiJaLiikuttaaHitboxia() {
         pelaaja.setNopeus(5);
@@ -36,9 +39,9 @@ public class PelaajaTest {
         pelaaja.liiku();
         assertEquals(105, pelaaja.getMaila().getY());
         assertEquals(100 - maila.getLeveys() / 2, maila.getHitbox().x);
-        assertEquals(105 - maila.getKorkeus() / 2, maila.getHitbox().y);    
+        assertEquals(105 - maila.getKorkeus() / 2, maila.getHitbox().y);
     }
-    
+
     @Test
     public void liikkuminenAlasToimiiJaLiikuttaaHitboxia() {
         pelaaja.setNopeus(5);
@@ -46,9 +49,9 @@ public class PelaajaTest {
         pelaaja.liiku();
         assertEquals(95, pelaaja.getMaila().getY());
         assertEquals(100 - maila.getLeveys() / 2, maila.getHitbox().x);
-        assertEquals(95 - maila.getKorkeus() / 2, maila.getHitbox().y);      
+        assertEquals(95 - maila.getKorkeus() / 2, maila.getHitbox().y);
     }
-    
+
     @Test
     public void kiihtyvyydenSaateleminenToimiiOikein() {
         assertEquals(0, pelaaja.getKiihtyvyys(), 0);
@@ -63,7 +66,7 @@ public class PelaajaTest {
         pelaaja.vahennaKiihtyvyytta();
         assertEquals(-1, pelaaja.getKiihtyvyys(), 0);
     }
-    
+
     @Test
     public void liikkuminenVahentaaKiihtyvyyttaOikein() {
         pelaaja.lisaaKiihtyvyytta();
@@ -77,5 +80,28 @@ public class PelaajaTest {
         pelaaja.liiku();
         assertEquals(0, pelaaja.getKiihtyvyys(), 0);
     }
-    
+
+    @Test
+    public void reagoiEriPoweruppeihinOikein() {
+        int mailanVanhaKorkeus = maila.getKorkeus();
+        pelaaja.reagoiPowerUpiin(PowerUp.MAILA_KASVAA);
+        assertEquals(mailanVanhaKorkeus + 20, maila.getKorkeus());
+
+        double vanhaNopeus = pelaaja.getNopeus();
+        pelaaja.reagoiPowerUpiin(PowerUp.MAILA_NOPEUTUU);
+        assertEquals(vanhaNopeus + 1, pelaaja.getNopeus(), 0);
+
+        pelaaja.reagoiPowerUpiin(PowerUp.MAILA_KASVAA);
+        pelaaja.reagoiPowerUpiin(PowerUp.MAILA_KASVAA);
+        pelaaja.reagoiPowerUpiin(PowerUp.MAILA_KASVAA);
+        pelaaja.reagoiPowerUpiin(PowerUp.MAILA_NOPEUTUU);
+        pelaaja.reagoiPowerUpiin(PowerUp.MAILA_NOPEUTUU);
+        pelaaja.reagoiPowerUpiin(PowerUp.MAILA_NOPEUTUU);
+        pelaaja.reagoiPowerUpiin(PowerUp.MAILA_NOPEUTUU);
+        pelaaja.reagoiPowerUpiin(PowerUp.RESETOI_POWERUPIT);
+
+        assertEquals(mailanVanhaKorkeus, maila.getKorkeus());
+        assertEquals(vanhaNopeus, pelaaja.getNopeus(), 0);
+    }
+
 }
